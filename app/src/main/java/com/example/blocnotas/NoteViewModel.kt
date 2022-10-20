7b774c21c9d3b48bf53f6f5701c93dbdcf3b2b99
R3inbow <the6javi@gmail.com>
2022-10-20 11:02:24 +0200

    Definido el viewModel y el FactoryViewModel

--- app/src/main/java/com/example/blocnotas/NoteViewModel.kt
+++ app/src/main/java/com/example/blocnotas/NoteViewModel.kt
@@ -2,11 +2,26 @@ package com.example.blocnotas
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.blocnotas.database.NoteDao
+import com.example.blocnotas.database.Notes
 
-class NoteViewModel: ViewModel() {
+class NoteViewModel(private val noteDao: NoteDao): ViewModel() {
 
     fun createNote (noteTitle: String, noteText: String){
         Log.d("newNote", "noteTitle= $noteTitle noteText= $noteText")
+    }
+
+    fun fullNotes(): List<Notes> = noteDao.getAll()
+}
+
+class NoteViewModelFactory(private val noteDao: NoteDao): ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(NoteViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return NoteViewModel(noteDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
 
